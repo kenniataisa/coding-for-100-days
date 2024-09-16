e85b98a4962f7ec0b24e64f2b1ce742b8526a787
kenniataisa <taisakennia@gmail.com>
2024-09-16 10:11:31 -0300

    dia 11

---
+++ dia11/script.js
@@ -0,0 +1,8 @@
+let box  = document.querySelector('.box');
+let input = document.querySelector('input');
+
+input.addEventListener('input', () => {
+    box.style.borderRadius = input.value; 
+    box.style.backgroundColor = input.value;
+
+})
